12b493182272ab6e4b08789b9829fc675da2daf6
zachSykes <sykes.zach12@gmail.com>
2018-03-05 01:53:36 -0500

    Program_2_18 commit

---
+++ src/com/mycompany/ass1b/chapter2/Program_2_18.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.mycompany.ass1b.chapter2;
+
+/**
+ * @author zacharysykes
+ *
+ */
+public class Program_2_18 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		/*
+		 * Write an application that displays
+		 *  a box, an oval, an arrow
+			and a diamond using asterisks (*), 
+			as follows in book pg. 65
+		 */
+		System.out.println("*********      ***        *         *");
+		System.out.println("*       *    *     *     ***       *  *");
+		System.out.println("*       *   *       *   *****     *    *");
+		System.out.println("*       *   *        *    *      *      *");
+		System.out.println("*       *   *        *    *     *        *");
+		System.out.println("*       *   *        *    *      *      *");
+		System.out.println("*       *   *        *    *       *    *");
+		System.out.println("*       *    *      *     *        *  *");
+		System.out.println("*********       ***       *          *");
+		
+	}
+
+}
