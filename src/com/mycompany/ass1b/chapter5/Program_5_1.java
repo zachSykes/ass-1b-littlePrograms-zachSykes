0f19d588780eb0b0dbee153040cb3225e694874d
zachSykes <sykes.zach12@gmail.com>
2018-03-05 16:42:13 -0500

    Program_5_1 commit

---
+++ src/com/mycompany/ass1b/chapter5/Program_5_1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.mycompany.ass1b.chapter5;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+/**
+ * @author zacharysykes
+ *
+ */
+public class Program_5_1 
+{
+	public static void main(String[] args)
+	{
+		int counter = 1;
+		while (counter <= 10)
+		{
+			System.out.printf("%d ", counter);
+			++counter;
+		}
+		System.out.println();
+}
+}
