75b32ee59c658016581fb3a0750abcb796505d90
zachSykes <sykes.zach12@gmail.com>
2018-03-05 11:09:49 -0500

    Program_4_15 commit

--- src/com/mycompany/ass1b/chapter4/Program_4_15.java
+++ src/com/mycompany/ass1b/chapter4/Program_4_15.java
@@ -12,8 +12,20 @@ public class Program_4_15 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
+		int c = 5;
+		System.out.printf("c before postincrement: %d%n", c);
+		System.out.printf("       postincrement c: %d%n", c++);
+		System.out.printf(" c after postincrement: %d%n", c);
+		
+		System.out.println();
+		
+		c = 5;
+		System.out.printf("c before preincrement: %d%n", c);
+		System.out.printf("       preincrement c: %d%n", ++c);
+		System.out.printf(" c after preincrement: %d%n", c);
 
 	}
 
