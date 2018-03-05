42e6f895ef6c40c831d03f76903f8778609c1600
zachSykes <sykes.zach12@gmail.com>
2018-03-05 11:01:24 -0500

    Program_4_10 commit

--- src/com/mycompany/ass1b/chapter4/Program_4_10.java
+++ src/com/mycompany/ass1b/chapter4/Program_4_10.java
@@ -2,6 +2,7 @@
  * 
  */
 package com.mycompany.ass1b.chapter4;
+import java.util.Scanner;
 
 /**
  * @author zacharysykes
@@ -12,8 +13,37 @@ public class Program_4_10 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int total = 0;
+		int gradeCounter = 0;
+		
+		System.out.print("Enter grade or -1 to quit: ");
+		int grade = input.nextInt();
+		
+		while (grade != -1)
+		{
+			total = total + grade;
+			gradeCounter = gradeCounter + 1;
+			
+			System.out.print("Enter grade or -1 to quit: ");
+			grade = input.nextInt();
+		}
+		
+		if (gradeCounter != 0)
+		{
+			double average = (double) total / gradeCounter;
+			
+			System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+			System.out.printf("Class average is %.2f%n", average);
+		}
+		else
+		{
+			System.out.println("No grades were entered");
+		}
 
 	}
 
