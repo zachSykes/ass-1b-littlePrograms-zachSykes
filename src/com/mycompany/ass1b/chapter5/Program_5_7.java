aa2e73b437546985e2b41b927c997f879db62ccd
zachSykes <sykes.zach12@gmail.com>
2018-03-05 16:46:13 -0500

    Program_5_7 commit

---
+++ src/com/mycompany/ass1b/chapter5/Program_5_7.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.mycompany.ass1b.chapter5;
+
+/**
+ * @author zacharysykes
+ *
+ */
+public class Program_5_7 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int counter = 1;
+		do 
+		{
+			System.out.printf("%d ", counter);
+			++counter;
+		} while (counter <= 10);
+		System.out.println();
+	}
+
+}
