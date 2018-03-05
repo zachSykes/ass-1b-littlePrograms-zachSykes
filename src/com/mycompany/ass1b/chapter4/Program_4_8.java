a056876ac827694124415b2e1ffbffbd548e3dae
zachSykes <sykes.zach12@gmail.com>
2018-03-05 10:52:14 -0500

    Program_4_8 commit

--- src/com/mycompany/ass1b/chapter4/Program_4_8.java
+++ src/com/mycompany/ass1b/chapter4/Program_4_8.java
@@ -2,11 +2,31 @@
  * 
  */
 package com.mycompany.ass1b.chapter4;
+import java.util.Scanner;
 
 /**
  * @author zacharysykes
  *
  */
-public class Program_4_8 {
-
+public class Program_4_8 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int total = 0;
+		int gradeCounter = 1;
+		
+		while (gradeCounter <= 10)
+		{
+			System.out.print("Enter grade: " );
+			int grade = input.nextInt();
+			total = total + grade;
+			gradeCounter = gradeCounter + 1;
+		}
+		
+		int average = total / 10;
+		System.out.printf("%nTotal of all 10 grades is %d%n", total);
+		System.out.printf("Class average is %d%n", average);
+	}
 }
